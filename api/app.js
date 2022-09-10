a51356d5625ad206d4546591a1f53e42fee53bc3
diamondrarktvo <amada.rktvo@gmail.com>
2022-09-10 13:35:56 +0300

    [IMP]configure routes

--- api/app.js
+++ api/app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const routeAuth = require('./routes/routeAuth');
+const routeExchange = require('./routes/routeExchange');
 const app = express();
 
 mongoose.connect(`mongodb+srv://dama:${process.env.PASS}@cluster-dama.qgpqn.mongodb.net/?retryWrites=true&w=majority`, {
@@ -17,9 +21,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 })
+app.use(bodyParser.json());
 
-app.use('/', (req, res) => {
-    res.json({message: 'OK'});
-})
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/v1/auth', routeAuth);
+app.use('/api/v1/exchange', routeExchange);
 
 module.exports = app;
